0df2ee2b926e534b4a8f31b40c575ad1bf589709
Anish Raj <anishrajkgg@gmail.com>
2026-06-24 21:45:39 +0530

    fix

--- src/main/java/com/hospital/Controller/controller.java
+++ src/main/java/com/hospital/Controller/controller.java
@@ -6,17 +6,19 @@ import com.hospital.dto.responseDto.DoctorResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/doctors")
+@RequestMapping("api/v1")
 public class controller {
 
     @Autowired
     private DoctorService doctorService;
 
+    @PostMapping("/addDoctors")
     public ResponseEntity<DoctorResponseDTO> addDoctor (@RequestBody DoctorRequestDTO doctorRequestDTO) {
         DoctorResponseDTO createDoctor = doctorService.createDoctor(doctorRequestDTO);
         return new ResponseEntity<>(createDoctor, HttpStatus.CREATED);
